74deb6655e0437876ca31af9ddd8c1a46a9cbe63
Ashyboban <138277106+Ashyboban@users.noreply.github.com>
2025-09-28 22:08:15 +0530

    Add files via upload

---
+++ task1/script.js
@@ -0,0 +1,47 @@
+const text1 = document.querySelector(".dynamic-text");
+const arraysentence = ["Welcome to Shopify", "Build your business", "Sell everywhere", "Market your brand"];
+let count = 0;
+
+function updateText() {
+  text1.textContent = arraysentence[count];
+  count++;
+  if(count >= arraysentence.length) {
+    count = 0; 
+  }
+}
+
+updateText();
+
+setInterval(updateText, 3000);
+
+const track = document.querySelector('.carousel-track');
+const cards = document.querySelectorAll('.card');
+const cardsPerSlide = 3;  
+let currentIndex = 0;
+
+function nextSlide() {
+  currentIndex += cardsPerSlide;
+
+  if (currentIndex >= cards.length) {
+    currentIndex = 0;
+  }
+
+  const cardWidth = cards[0].getBoundingClientRect().width;
+  track.style.transform = `translateX(-${cardWidth * currentIndex}px)`;
+}
+
+setInterval(nextSlide, 3000);
+
+
+const images = document.querySelectorAll(".card img");
+
+images.forEach(img => {
+  img.addEventListener("mouseover", () => {
+    img.src = img.dataset.hover;   
+  });
+
+  img.addEventListener("mouseout", () => {
+    img.src = img.dataset.original; 
+  });
+});
+
